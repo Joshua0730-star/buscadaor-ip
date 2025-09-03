808b3f51ea419bfdaf52a7cc8fef65354ce52918
Josue Marchena <josuemarchena0730@gmail.com>
2025-09-02 20:31:19 -0500

    Primer ensamble del proyecto

---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+export const OPTIONS = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "546e49f6admsh7928d158e64840cp1f26afjsn86dfb7ce30a7",
+    "x-rapidapi-host": "ip-lookup-threat-detection-api.p.rapidapi.com",
+  },
+};
