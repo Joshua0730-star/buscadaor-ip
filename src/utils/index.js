7d086d63bf8e6a03feb784cbf51c6a4545f01da5
Josue Marchena <josuemarchena0730@gmail.com>
2025-09-22 11:29:44 -0500

    añadiendo otra clave

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,9 @@
-import { API_KEY } from "astro:env/client";
+import { RAPID_API_KEY } from "astro:env/client";
 
 export const OPTIONS = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": API_KEY,
-    "x-rapidapi-host": "ip-geo-location10.p.rapidapi.com",
+    "x-rapidapi-key": RAPID_API_KEY,
+    "x-rapidapi-host": "ip-geolocation-api6.p.rapidapi.com",
   },
 };
